2b44db77e82e5839bec5b9ba46c7590a8def0e62
Stephen Camilo <stephen.camilo@live.com>
2020-05-18 02:29:14 -0300

    subindo projeto

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $('#buscar').click(function () {
-    var buscar = $('#buscar-input').val();
-    window.location.href = "http://" + window.location.hostname + "/pokemon/" + buscar;
-  });
-});
